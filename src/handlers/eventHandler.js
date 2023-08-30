cb4dea50feb9fb6a2dc156a65072a570863b2e54
PGM-vivo <acs109142@gm.ntcu.edu.tw>
2023-08-30 18:27:14 +0800

    add comment in eventHandler

--- src/handlers/eventHandler.js
+++ src/handlers/eventHandler.js
@@ -1,16 +1,18 @@
 const path = require('path');
-const getAllFiles = require('../utils/getAllFiles');
+const getAllFiles = require('@/utils/getAllFiles');
 
 module.exports = (client) => {
   const eventFolders = getAllFiles(path.join(__dirname, '..', 'events'), true);
 
   for (const eventFolder of eventFolders) {
-    const eventFiles = getAllFiles(eventFolder);
+    const eventFiles = getAllFiles(eventFolder); // string array
     eventFiles.sort((a, b) => a > b);
 
     const eventName = eventFolder.replace(/\\/g, '/').split('/').pop();
 
-    client.on(eventName, async (arg) => {
+    // When discord.js event emitter emit an `eventName` event, run all exported function inside the folder.
+    // eventName might be 'ready', 'messageCreate' or 'interactionCreate'. Cannot change.
+    client.on(eventName, async (arg) => { // arg would be emitted by discord.js
       for (const eventFile of eventFiles) {
         const eventFunction = require(eventFile);
         await eventFunction(client, arg);
